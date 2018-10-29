0e8b1b385d3840eed6344300c1631b06a2525171
123tarunanand <anandtarun2@gmail.com>
2018-10-29 19:36:34 +0530

    Frontend fixes

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/static'));
 app.use(favicon(__dirname +'/img/cr.ico'));
-
+app.use(function(req, res, next) {
+  res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+  next();
+});
 app.engine('html', require('ejs').renderFile);
 app.use(session({
   cookieName: 'session',
